384e9fff1ac2ec8f0be910dad46c2becb067f093
goody09083729531 <wobogoodnessg@gmail.com>
2023-05-21 01:35:30 +0100

    init

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -7,16 +7,24 @@ import { useStateContext } from '../context/StateContext';
 const Navbar = () => {
   const { showCart, setShowCart, totalQuantities } = useStateContext();
 
+  const myElementRef = useRef(null);
+  const timesRef = useRef(null);
+
+  const handleClick = () => {
+    myElementRef.current.classList.toggle('active');
+    timesRef.current.classList.toggle('fa-times');
+  };
+
   return (
     <header>
       <a className="logo">Nsikak</a>
-      <nav className="navbar">
+      <nav className="navbar" ref={myElementRef}>
         <Link href="/" className="active link">Home</Link>
         <Link href="/product/shop" className="link">Shop</Link>
         <Link href="" className="link">Contact</Link>
       </nav>
       <div className="icons">
-        <i className="fas fa-bars" id="menu-bars"></i>
+        <i className="fas fa-bars" id="menu-bars" ref={timesRef} onClick={handleClick}></i>
         <i className="fab fa-facebook-f"></i>
         <i className="fab fa-twitter"></i>
         <i className="fa-solid fa-cart-shopping" onClick={() => setShowCart(true)}></i>
